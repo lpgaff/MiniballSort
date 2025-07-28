7e63d7f4d3fccbd69de72de56c306f1894767c95
lpgaff <lpgaff@gmail.com>
2025-07-28 11:43:51 +0000

    Deploying to gh-pages from @ lpgaff/MiniballSort@7653e319ad9a67f4698b3a5d23c11f58fc9d4d4e 🚀

--- search/functions_4.js
+++ search/functions_4.js
@@ -5,5 +5,7 @@ var searchData=
   ['ejectilecut_2',['EjectileCut',['../classMiniballHistogrammer.html#a3e3a139baece36949d85b8082dee998d',1,'MiniballHistogrammer']]],
   ['eventscdpadcoincidence_3',['EventsCdPadCoincidence',['../classMiniballReaction.html#acba19b42232b1cd8ad17ad54fdcbf077',1,'MiniballReaction']]],
   ['eventscdpadveto_4',['EventsCdPadVeto',['../classMiniballReaction.html#a45e158408c5c1c59d514dd35676f3806',1,'MiniballReaction']]],
-  ['eventsparticlegammaonly_5',['EventsParticleGammaOnly',['../classMiniballReaction.html#ac3e6ed977543da3dd6e278d7dc0451f7',1,'MiniballReaction']]]
+  ['eventsgammademandsegment_5',['EventsGammaDemandSegment',['../classMiniballReaction.html#a3bd55b3ab73b0c7f8e3a1c9755f9e861',1,'MiniballReaction']]],
+  ['eventsgammamaxsegmentmultiplicity_6',['EventsGammaMaxSegmentMultiplicity',['../classMiniballReaction.html#acfcc4c4182e39f31da6ad69aa6724f22',1,'MiniballReaction']]],
+  ['eventsparticlegammaonly_7',['EventsParticleGammaOnly',['../classMiniballReaction.html#ac3e6ed977543da3dd6e278d7dc0451f7',1,'MiniballReaction']]]
 ];
